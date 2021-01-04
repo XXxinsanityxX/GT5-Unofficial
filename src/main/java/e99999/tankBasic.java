e842dec1214d0aad535fa13f832cc725ecb8a6c3
XXxinsanityxX <42390377+XXxinsanityxX@users.noreply.github.com>
2021-01-03 23:54:28 -0600

    Tanks auto output modes

--- src/main/java/e99999/tankBasic.java
+++ src/main/java/e99999/tankBasic.java
@@ -6,20 +6,32 @@ import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicTank;
 import gregtech.api.objects.GT_RenderedTexture;
+import gregtech.api.util.GT_Utility;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidHandler;
 
-public class tankBasic
-        extends GT_MetaTileEntity_BasicTank {
+public class tankBasic extends GT_MetaTileEntity_BasicTank {
+    public boolean OutputFluid = false;
+
     public tankBasic(int aID, String aName, String aNameRegional, int aTier) {
-        super(aID, aName, aNameRegional, aTier, 3, "Stores " + ((int) (Math.pow(2, aTier) * 16000)) + "L of fluid & outputs front, melts at internal pipe temp");
+        super(aID, aName, aNameRegional, aTier, 3, new String[]{
+                "Stores " + CommonSizeCompute(aTier) + "L of fluid",
+                "Melts above the internal pipe temperature",
+                "Use Shift + Screwdriver to enable auto output mode"});
     }
 
     public tankBasic(String aName, int aTier, String aDescription, ITexture[][][] aTextures) {
     	super(aName, aTier, 3, aDescription, aTextures);
+    }
+
+    @Override
+    public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
+        return new tankBasic(mName, mTier, mDescription, mTextures);
     }
 
     @Override
@@ -29,18 +41,97 @@ public class tankBasic
 
     @Override
     public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
-        return aSide == aFacing ? new ITexture[]{Textures.BlockIcons.STORAGESTUFF[mTier][aColorIndex + 1], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PIPE_OUT)} : new ITexture[]{Textures.BlockIcons.STORAGESTUFF[mTier][aColorIndex + 1]};
+        return aSide != aFacing
+                ? aSide == 1
+                ? new ITexture[]{Textures.BlockIcons.STORAGESTUFF[mTier][aColorIndex + 1]}
+                : new ITexture[]{Textures.BlockIcons.STORAGESTUFF[mTier][aColorIndex + 1]}
+                : aActive
+                ? getTexturesActive(Textures.BlockIcons.STORAGESTUFF[mTier][aColorIndex + 1])
+                : getTexturesInactive(Textures.BlockIcons.STORAGESTUFF[mTier][aColorIndex + 1]);
     }
 
-    @Override
+    public ITexture[] getTexturesActive(ITexture aBaseTexture) {
+        return new ITexture[]{aBaseTexture, new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PIPE_OUT)};
+    }
+
+    public ITexture[] getTexturesInactive(ITexture aBaseTexture) {
+        return new ITexture[]{aBaseTexture, new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PIPE_OUT)};
+    }
+
+    public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
+        super.onPostTick(aBaseMetaTileEntity, aTick);
+        //if (this.getBaseMetaTileEntity().isServerSide() && (aTick&0x7)==0) {
+        if (getDrainableStack() != null){
+            IFluidHandler tTank = aBaseMetaTileEntity.getITankContainerAtSide(aBaseMetaTileEntity.getFrontFacing());
+            if (tTank != null) {
+                if (this.OutputFluid) {
+                    FluidStack tDrained = drain(ForgeDirection.getOrientation(aBaseMetaTileEntity.getFrontFacing()), 256, false);
+                    if (tDrained != null) {
+                        int tFilledAmount = tTank.fill(ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()), tDrained, false);
+                        if (tFilledAmount > 0) {
+                            tTank.fill(ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()), aBaseMetaTileEntity.drain(ForgeDirection.getOrientation(aBaseMetaTileEntity.getFrontFacing()), tFilledAmount, true), true);
+                        }
+                    }
+                }
+            }
+        }
+        if (mFluid != null && mFluid.amount > 0) {
+            int tLimit = 350; //default limit
+            if (mTier == 1) {
+                tLimit = 2000;
+            } else if (mTier == 2) {
+                tLimit = 2500;
+            } else if (mTier == 3) {
+                tLimit = 3000;
+            } else if (mTier == 4) {
+                tLimit = 5000;
+            }
+
+            int tTemperature = mFluid.getFluid().getTemperature(mFluid);
+            if (tTemperature > tLimit) {
+                if (aBaseMetaTileEntity.getRandomNumber(100) == 0) {
+                    aBaseMetaTileEntity.setToFire();
+                    return;
+                }
+                aBaseMetaTileEntity.setOnFire();
+            }
+        }
+    }
+
     public void saveNBTData(NBTTagCompound aNBT) {
+        aNBT.setBoolean("OutputFluid", this.OutputFluid);
         super.saveNBTData(aNBT);
+    }
+
+    public void loadNBTData(NBTTagCompound aNBT) {
+        super.loadNBTData(aNBT);
+        this.OutputFluid = aNBT.getBoolean("OutputFluid");
+    }
+
+    public void onScrewdriverRightClick(byte aSide, EntityPlayer aPlayer, float aX, float aY, float aZ) {
+        super.onScrewdriverRightClick(aSide, aPlayer, aX, aY, aZ);
+        this.OutputFluid = !OutputFluid;
+        if (!this.OutputFluid) {
+            GT_Utility.sendChatToPlayer(aPlayer, "Fluid Output Disabled");
+        } else {
+            GT_Utility.sendChatToPlayer(aPlayer, "Fluid Output Enabled");
+        }
     }
 
     @Override
     public boolean onRightclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer) {
         if (aBaseMetaTileEntity.isClientSide()) return true;
         aBaseMetaTileEntity.openGUI(aPlayer);
+        return true;
+    }
+
+    @Override
+    public boolean allowPullStack(IGregTechTileEntity aBaseMetaTileEntity, int aIndex, byte aSide, ItemStack aStack) {
+        return true;
+    }
+
+    @Override
+    public boolean allowPutStack(IGregTechTileEntity aBaseMetaTileEntity, int aIndex, byte aSide, ItemStack aStack) {
         return true;
     }
 
@@ -60,23 +151,18 @@ public class tankBasic
     }
 
     @Override
-    public void loadNBTData(NBTTagCompound aNBT) {
-        super.loadNBTData(aNBT);
-    }
-
-    @Override
     public final byte getUpdateData() {
         return 0x00;
     }
 
     @Override
     public boolean doesFillContainers() {
-    	return true;
+        return true;
     }
 
     @Override
     public boolean doesEmptyContainers() {
-    	return true;
+        return true;
     }
 
     @Override
@@ -84,48 +170,6 @@ public class tankBasic
         return true;
     }
 
-    @Override
-    public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
-        super.onPostTick(aBaseMetaTileEntity, aTick);
-        if (getDrainableStack() != null){
-            IFluidHandler tTank = aBaseMetaTileEntity.getITankContainerAtSide(aBaseMetaTileEntity.getFrontFacing());
-            if (tTank != null) {
-                FluidStack tDrained = drain(250, false);
-                if (tDrained != null) {
-                    int tFilledAmount = tTank.fill(ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()), tDrained, false);
-                    if (tFilledAmount > 0)
-                        tTank.fill(ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()), drain(tFilledAmount, true), true);
-                }
-            }
-
-        }
-
-        if (mFluid != null && mFluid.amount > 0) {
-            int tLimit = 350; //default limit
-        	if (mTier == 1) {
-        		tLimit = 2000;
-        	} else if (mTier == 2) {
-        		tLimit = 2500;
-        	} else if (mTier == 3) {
-        		tLimit = 5000;
-        	} else if (mTier == 4) {
-        		tLimit = 12500;
-        	}
-        
-        	int tTemperature = mFluid.getFluid().getTemperature(mFluid);
-        	if (tTemperature > tLimit) {
-        		if (aBaseMetaTileEntity.getRandomNumber(100) == 0) {
-        			aBaseMetaTileEntity.setToFire();
-        			return;
-        		}
-        		aBaseMetaTileEntity.setOnFire();
-        	}
-
-    	}
-    }
-    
-    
-    
     @Override
     public boolean canTankBeEmptied() {
         return true;
@@ -146,18 +190,18 @@ public class tankBasic
 
         if (mFluid == null) {
             return new String[]{
-                    "Basic Tank",
-                    "Stored Fluid:",
-                    "No Fluid",
-                    Integer.toString(0) + "L",
-                    Integer.toString(getCapacity()) + "L"};
+                EnumChatFormatting.BLUE + "Basic Tank"+ EnumChatFormatting.RESET, "Stored Fluid:",
+                EnumChatFormatting.GOLD + "No Fluid"+ EnumChatFormatting.RESET,
+                EnumChatFormatting.GREEN + Integer.toString(0) + " L"+ EnumChatFormatting.RESET+" "+
+                EnumChatFormatting.YELLOW + Integer.toString(getCapacity()) + " L"+ EnumChatFormatting.RESET
+            };
         }
         return new String[]{
-                "Basic Tank",
-                "Stored Fluid:",
-                mFluid.getLocalizedName(),
-                Integer.toString(mFluid.amount) + "L",
-                Integer.toString(getCapacity()) + "L"};
+                EnumChatFormatting.BLUE + "Basic Tank"+ EnumChatFormatting.RESET, "Stored Fluid:",
+                EnumChatFormatting.GOLD + mFluid.getLocalizedName()+ EnumChatFormatting.RESET,
+                EnumChatFormatting.GREEN + Integer.toString(mFluid.amount) + " L"+ EnumChatFormatting.RESET+" "+
+                EnumChatFormatting.YELLOW+ Integer.toString(getCapacity()) + " L"+ EnumChatFormatting.RESET
+        };
     }
 
     @Override
@@ -165,19 +209,30 @@ public class tankBasic
         return true;
     }
 
-    @Override
-    public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
-        return new tankBasic(mName, mTier, mDescription, mTextures);
+    private static int CommonSizeCompute(int tier){
+        switch(tier){
+            case 0:
+                return  16000;
+            case 1:
+                return  32000;
+            case 2:
+                return  64000;
+            case 3:
+                return 128000;
+            case 4:
+                return 256000;
+            default:
+                return 0;
+        }
     }
 
     @Override
     public int getCapacity() {
-        return (int) (Math.pow(2, mTier) * 16000);
+        return CommonSizeCompute(mTier);
     }
 
     @Override
     public int getTankPressure() {
         return 100;
     }
-
-} 
+}
--- src/main/java/gregtech/common/tileentities/storage/GT_MetaTileEntity_QuantumTank.java
+++ src/main/java/gregtech/common/tileentities/storage/GT_MetaTileEntity_QuantumTank.java
@@ -6,13 +6,22 @@ import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicTank;
 import gregtech.api.objects.GT_RenderedTexture;
+import gregtech.api.util.GT_Utility;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidHandler;
 
-public class GT_MetaTileEntity_QuantumTank
-        extends GT_MetaTileEntity_BasicTank {
+public class GT_MetaTileEntity_QuantumTank extends GT_MetaTileEntity_BasicTank {
+    public boolean OutputFluid = false;
+
     public GT_MetaTileEntity_QuantumTank(int aID, String aName, String aNameRegional, int aTier) {
-        super(aID, aName, aNameRegional, aTier, 3, "Stores " + ((int) (Math.pow(6, aTier) * 267000)) + "L of fluid");
+        super(aID, aName, aNameRegional, aTier, 3, new String[]{
+                "Stores " + CommonSizeCompute(aTier) + "L of fluid",
+                "Use Shift + Screwdriver to enable auto output mode"});
     }
 
     public GT_MetaTileEntity_QuantumTank(String aName, int aTier, String aDescription, ITexture[][][] aTextures) {
@@ -24,24 +33,87 @@ public class GT_MetaTileEntity_QuantumTank
     }
 
     @Override
+    public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
+        return new GT_MetaTileEntity_QuantumTank(mName, mTier, mDescription, mTextures);
+    }
+
+    @Override
     public ITexture[][][] getTextureSet(ITexture[] aTextures) {
         return new ITexture[0][0][0];
     }
 
     @Override
     public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
-        return aSide == 1 ? new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_QTANK)} : new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1]};
+        return aSide != aFacing
+                ? aSide == 1
+                ? new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_QTANK)}
+                : new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1]}
+                : aActive
+                ? getTexturesActive(Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1])
+                : getTexturesInactive(Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1]);
     }
 
-    @Override
+    public ITexture[] getTexturesActive(ITexture aBaseTexture) {
+        return new ITexture[]{aBaseTexture, new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PIPE_OUT)};
+    }
+
+    public ITexture[] getTexturesInactive(ITexture aBaseTexture) {
+        return new ITexture[]{aBaseTexture, new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_PIPE_OUT)};
+    }
+
+    public void onPostTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
+        super.onPostTick(aBaseMetaTileEntity, aTick);
+        //if (this.getBaseMetaTileEntity().isServerSide() && (aTick&0x7)==0) {
+        if (getDrainableStack() != null){
+            IFluidHandler tTileEntity = aBaseMetaTileEntity.getITankContainerAtSide(aBaseMetaTileEntity.getFrontFacing());
+            if (tTileEntity != null) {
+                if (this.OutputFluid) {
+                    FluidStack tDrained = aBaseMetaTileEntity.drain(ForgeDirection.getOrientation(aBaseMetaTileEntity.getFrontFacing()), 256000, false);
+                    if (tDrained != null) {
+                        int tFilledAmount = tTileEntity.fill(ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()), tDrained, false);
+                        if (tFilledAmount > 0) {
+                            tTileEntity.fill(ForgeDirection.getOrientation(aBaseMetaTileEntity.getBackFacing()), aBaseMetaTileEntity.drain(ForgeDirection.getOrientation(aBaseMetaTileEntity.getFrontFacing()), tFilledAmount, true), true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     public void saveNBTData(NBTTagCompound aNBT) {
+        aNBT.setBoolean("OutputFluid", this.OutputFluid);
         super.saveNBTData(aNBT);
+    }
+
+    public void loadNBTData(NBTTagCompound aNBT) {
+        super.loadNBTData(aNBT);
+        this.OutputFluid = aNBT.getBoolean("OutputFluid");
+    }
+
+    public void onScrewdriverRightClick(byte aSide, EntityPlayer aPlayer, float aX, float aY, float aZ) {
+        super.onScrewdriverRightClick(aSide, aPlayer, aX, aY, aZ);
+        this.OutputFluid = !OutputFluid;
+        if (!this.OutputFluid) {
+            GT_Utility.sendChatToPlayer(aPlayer, "Fluid Output Disabled");
+        } else {
+            GT_Utility.sendChatToPlayer(aPlayer, "Fluid Output Enabled");
+        }
     }
 
     @Override
     public boolean onRightclick(IGregTechTileEntity aBaseMetaTileEntity, EntityPlayer aPlayer) {
         if (aBaseMetaTileEntity.isClientSide()) return true;
         aBaseMetaTileEntity.openGUI(aPlayer);
+        return true;
+    }
+
+    @Override
+    public boolean allowPullStack(IGregTechTileEntity aBaseMetaTileEntity, int aIndex, byte aSide, ItemStack aStack) {
+        return true;
+    }
+
+    @Override
+    public boolean allowPutStack(IGregTechTileEntity aBaseMetaTileEntity, int aIndex, byte aSide, ItemStack aStack) {
         return true;
     }
 
@@ -58,11 +130,6 @@ public class GT_MetaTileEntity_QuantumTank
     @Override
     public boolean isAccessAllowed(EntityPlayer aPlayer) {
         return true;
-    }
-
-    @Override
-    public void loadNBTData(NBTTagCompound aNBT) {
-        super.loadNBTData(aNBT);
     }
 
     @Override
@@ -105,18 +172,18 @@ public class GT_MetaTileEntity_QuantumTank
 
         if (mFluid == null) {
             return new String[]{
-                    "Quantum Tank",
-                    "Stored Fluid:",
-                    "No Fluid",
-                    Integer.toString(0) + "L",
-                    Integer.toString(getCapacity()) + "L"};
+                EnumChatFormatting.BLUE + "Quantum Tank"+ EnumChatFormatting.RESET, "Stored Fluid:",
+                EnumChatFormatting.GOLD + "No Fluid"+ EnumChatFormatting.RESET,
+                EnumChatFormatting.GREEN + Integer.toString(0) + " L"+ EnumChatFormatting.RESET+" "+
+                EnumChatFormatting.YELLOW + Integer.toString(getCapacity()) + " L"+ EnumChatFormatting.RESET
+            };
         }
         return new String[]{
-                "Quantum Tank",
-                "Stored Fluid:",
-                mFluid.getLocalizedName(),
-                Integer.toString(mFluid.amount) + "L",
-                Integer.toString(getCapacity()) + "L"};
+                EnumChatFormatting.BLUE + "Quantum Tank"+ EnumChatFormatting.RESET, "Stored Fluid:",
+                EnumChatFormatting.GOLD + mFluid.getLocalizedName()+ EnumChatFormatting.RESET,
+                EnumChatFormatting.GREEN + Integer.toString(mFluid.amount) + " L"+ EnumChatFormatting.RESET+" "+
+                EnumChatFormatting.YELLOW+ Integer.toString(getCapacity()) + " L"+ EnumChatFormatting.RESET
+        };
     }
 
     @Override
@@ -124,14 +191,26 @@ public class GT_MetaTileEntity_QuantumTank
         return true;
     }
 
-    @Override
-    public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
-        return new GT_MetaTileEntity_QuantumTank(mName, mTier, mDescriptionArray, mTextures);
+    private static int CommonSizeCompute(int tier){
+        switch(tier){
+            case 1:
+                return  1602000 ;
+            case 2:
+                return  9612000 ;
+            case 3:
+                return  57672000 ;
+            case 4:
+                return 346032000;
+            case 5:
+                return 2076192000 ;
+            default:
+                return 0;
+        }
     }
 
     @Override
     public int getCapacity() {
-        return (int) (Math.pow(6, mTier) * 267000);
+        return CommonSizeCompute(mTier);
     }
 
     @Override
